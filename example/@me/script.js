f29c9bc827da7d7b87262eff156c8762d352d99f
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-08-03 21:39:35 +0200

    Update script.js

--- example/@me/script.js
+++ example/@me/script.js
@@ -1,16 +1,13 @@
-window.onload = () => {
+auth.onAuthStateChanged(user) => {
     const maestro = document.getElementById("master-div");
-    const user = firebase.auth().currentUser;
+    
     if(user){
-        store.collection("c").where("id", "==", user.uid)
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    const lava = doc.data()
-                    maestro.innerHTML = `<p>Editing ${lava.username}'s Profile</p><hr><input type="url" placeholder="pfp"></input><input type="url" placeholder="banner"></input><input disabled="true" type="text" placeholder="${lava.username}"></input><hr><button onclick="updata()">Save</button><button onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${lava.username}';">Return to Channel</button>`;
-            });
-        })
-    } else {
-        window.alert('no auth lmao');
+      store.collection('c').get().then(snapshot => {
+      setupUI(snapshot.docs, user);
+    }, err => {
+      window.alert(err.message)
+    });
+    } else{
+        console.log('no auth lmao');
     }
-}
+})
