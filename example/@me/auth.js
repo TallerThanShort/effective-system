5b29c1bce97e74718653d8f62d0c9f113bf00709
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-08-04 13:11:13 +0200

    authorise me already

---
+++ example/@me/auth.js
@@ -0,0 +1,11 @@
+auth.onAuthStateChanged(user) => {
+    if(user){
+      store.collection('c').get().then(snapshot => {
+      setupUI(user);
+    }, err => {
+      window.alert(err.message)
+    });
+    } else{
+        window.alert('nonauth')
+    }
+})
