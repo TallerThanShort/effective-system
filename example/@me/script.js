61ed627220af6b4c69bbe4a7eaad1b77f88da747
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-08-29 11:44:04 +0200

    pls fix 2

--- example/@me/script.js
+++ example/@me/script.js
@@ -3,8 +3,10 @@ const maestro = document.getElementById("master-div");
 const setupUI = (user) => {
   if(user){
     store.collection('c').doc(user.uid).get().then(doc => {
+      const lava = doc.data();
       const html = `<p>Editing ${lava.username}'s Profile</p><hr><input type="url" placeholder="pfp"></input><input type="url" placeholder="banner"></input><input disabled="true" type="text" placeholder="${lava.username}"></input><hr><button onclick="updata()">Save</button><button onclick="location.href='https://common-codes.github.io/OpenSource-uTube/c/#user=${lava.username}';">Return to Channel</button>`;
       maestro.innerHTML = html;
+      console.log(auth.currentUser.uid);
     })
   } else {
     maestro.innerHTML = '<h5 style="color: crimson;">Log In</h5>'
